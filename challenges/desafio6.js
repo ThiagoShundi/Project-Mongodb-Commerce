af8a48e07e0df930a1c37b0df9b57c3bd4fcf5e0
ThiagoShundi <shundi_hayashi@hotmail.com>
2023-04-17 22:34:42 -0300

    Requisito 6 ao 10 Finalizados.

---
+++ challenges/desafio6.js
@@ -0,0 +1,13 @@
+db.produtos.find({
+    $or: [{
+            curtidas: {
+                $gte: 10,
+                $lt: 100,
+            }
+        },
+    ]
+}, {
+    nome: 1,
+    curtidas: 1,
+    _id: 0
+});
